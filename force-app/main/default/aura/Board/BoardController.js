dbc7ebf24cdca3bdf18b20fc10c694c4afd84c87
Tim Meehan <meehanti23@gmail.com>
2023-10-08 16:42:54 -0400

    connect board components

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,11 +1,23 @@
 ({
     doInit : function(component, event, helper) {
-        const words = helper.getWords(6);
+
+        const gameMode = component.get("v.mode");
+        let column = 0;
+        if(gameMode && gameMode === "hard") {
+            column = 6;
+        } else if(gameMode === "medium") {
+            column = 4;
+        } else {
+            column = 3;
+        }
+
+        let blockSize = 12/column;
+        component.set("v.blockSize", blockSize);
+
+        const words = helper.getWords(column * column);
         component.set("v.words", words);
 
         const winWord = helper.getWinWord(words);
         component.set("v.winWord", winWord);
-
-        console.log(winWord+'-----'+words);
     }
 })
